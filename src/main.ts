3aec00267dff56dd32047d1be0c8cfe281472428
Kafil Nasdami <k.nasdami@externe.labelvie.ma>
2024-09-25 19:31:24 +0100

    chore: remove api prefix

--- src/main.ts
+++ src/main.ts
@@ -12,14 +12,10 @@ if (process.env.NODE_ENV !== 'production') {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
   initSwagger(app);
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}}`);
 }
 
 bootstrap();
