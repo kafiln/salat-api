bc11dfba6e9ed3f8d58694a7db534766dc62aae5
Kafil Nasdami <k.nasdami@externe.labelvie.ma>
2024-09-25 20:11:38 +0100

    chore: move swagger to homepage

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
-import { initSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe());
-  initSwagger(app);
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}}`);
 }
